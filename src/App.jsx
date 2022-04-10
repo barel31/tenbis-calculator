df51ed3c9ca6f05d36ad62dc040d88892ce96d3b
Barel Shraga <barel123133@gmail.com>
2022-04-10 16:31:07 +0300

    Add wage title and shekels sign

--- src/App.jsx
+++ src/App.jsx
@@ -6,32 +6,36 @@ import './App.css';
 
 function App() {
     const [vehicle, setVehicle] = useState('Motorcycle');
-    const [timeStart, setTimeStart] = useState('');
-    const [timeEnd, setTimeEnd] = useState('');
+    const [timeStart, setTimeStart] = useState('00:00');
+    const [timeEnd, setTimeEnd] = useState('00:00');
     const [deliverysNoon, setDeliverysNoon] = useState(0);
     const [deliverysEvening, setDeliverysEvening] = useState(0);
     const [tips, setTips] = useState(0);
 
     const today = new Date();
-    const currentDay = today.toLocaleString('he-IL', { weekday: 'long', day: 'numeric', month: 'long', hour: '2-digit', minute: '2-digit' });
+    const currentDay = today.toLocaleString('he-IL', {
+        weekday: 'long',
+        day: 'numeric',
+        month: 'long',
+        hour: '2-digit',
+        minute: '2-digit',
+    });
 
     const calc = () => {
         let hour = 30; // base
-        hour += vehicle === 'Motorcycle' ? 20 : 15; // vehicle bonus
+        hour += vehicle === 'Motorcycle' ? 20 : 15; // vehicle bonus + noon bonus
 
         const timeStartDec = hoursToDec(timeStart); // start time
         const timeEndDec = hoursToDec(timeEnd); // end time
 
         const hours = timeEndDec - timeStartDec; // number of hours
 
-        let NoBonus = 0;
+        let NoBonus = 0; // reduce bonus after noon
         if (timeEndDec >= 15) {
             NoBonus = 5;
             NoBonus *= timeEndDec - 15;
         }
-        console.log(`bonus = ${NoBonus} => ${timeEndDec} - 15`);
-        // NoBonus *= hours;
-        // console.log(`bonus after multi = ${NoBonus}`);
+        // calc wage
         const wage = hours * hour + (tips | 0) - NoBonus + deliverysNoon * 8 + deliverysEvening * 5;
 
         return (
@@ -41,7 +45,7 @@ function App() {
                 <p>wage: {hours * hour}</p>
                 <p>dec start: {timeStartDec}</p>
                 <p>dec end: {timeEndDec}</p>
-                <p>wage: {wage}</p>
+                <h3>wage: {wage}₪</h3>
             </>
         );
     };
@@ -51,19 +55,6 @@ function App() {
             str.split(':').reduce((seconds, v) => {
                 return +v + seconds * 60;
             }, 0) / 60
-        );
-    };
-
-    const tests = () => {
-        return (
-            <>
-                <p>{vehicle}</p>
-                <p>start: {timeStart}</p>
-                <p>end: {timeEnd}</p>
-                <p>deliverys noon: {deliverysNoon}</p>
-                <p>deliverys evening: {deliverysEvening}</p>
-                <p>tips: {tips}</p>
-            </>
         );
     };
 
@@ -87,26 +78,22 @@ function App() {
             <label htmlFor='time_end'>Shift ends: </label>
             <input name='time_end' type='time' placeholder='Shift ends' onChange={(e) => setTimeEnd(e.target.value)} />
 
-            <label htmlFor='deliverys_noon'>Delivery peak noon: (11:30-13:30)</label>
+            <label>Deliveries peak: </label>
             <input
                 name='deliverys_noon'
                 type='number'
-                placeholder='Delivery peak noon'
+                placeholder='Peak noon (11:30-13:30)'
                 onChange={(e) => setDeliverysNoon(e.target.value)}
             />
-
-            <label htmlFor='deliverys_evening'>Delivery peak noon: </label>
             <input
                 name='deliverys_evening'
                 type='number'
-                placeholder='Delivery peak evening'
+                placeholder='Peak evening (19:30-21:30)'
                 onChange={(e) => setDeliverysEvening(e.target.value)}
             />
 
             <label htmlFor='tips'>Tips: </label>
             <input name='tips' type='number' placeholder='Tips' onChange={(e) => setTips(e.target.value)} />
-
-            {tests()}
             {calc()}
         </div>
     );
